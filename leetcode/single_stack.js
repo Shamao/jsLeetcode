26d91d1d1718460dad684210543a5801a58b2d17
刘赛登 <liusd@tuya.com>
2020-11-24 18:48:52 +0800

    临时提交

---
+++ leetcode/single_stack.js
@@ -0,0 +1,27 @@
+// 比如说，输入一个数组 nums = [2,1,2,4,3]，你返回数组 [4,2,4,-1,-1]。
+
+const a = [2, 3, 2, 4, 3]
+const ret = []
+const temp = []
+
+console.log(a)
+for (let i = a.length - 1; i >= 0; i--) {
+  while (temp.length > 0 && temp[temp.length - 1] <= a[i]) {
+    temp.pop()
+  }
+  ret[i] = temp.length == 0 ? -1 : temp[temp.length - 1]
+  temp.push(a[i])
+
+  console.log(`index = ${i} 结果${ret[i]}  , 临时素组${temp}`)
+}
+
+console.log(ret)
+
+/**
+ * 按
+ * 
+ * index = 5的时候 temp为空 ret为 -1， temp加入最后一项 temp = 【3】
+ * index = 4的    temp不为空， 进入while循环，清temp  temp  = 【4】
+ * index = 3， temp 不为空， cur  = 2 < 4 不进入渲染， 得ret 结果 4， temp = 【4， 2】
+ * 以此类推
+ */
