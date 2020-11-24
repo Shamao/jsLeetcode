f929125a9cd742b3c86575a292bc60a4223f05f5
刘赛登 <liusd@tuya.com>
2020-11-24 12:02:51 +0800

    临时提交

--- src/l2.js
+++ src/l2.js
@@ -1,7 +1,4 @@
 // 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
-
-const { head } = require("lodash");
-
 // 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
 
 // 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
@@ -14,7 +11,7 @@ const { head } = require("lodash");
 
 // ListNode pre = new ListNode(0);
 // ListNode cur = pre;
-// int carry = 0;
+// int carry = 0; // 进位
 // while(l1 != null || l2 != null) {
 //     int x = l1 == null ? 0 : l1.val;
 //     int y = l2 == null ? 0 : l2.val;
