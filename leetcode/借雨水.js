cde9f0dde51b3300f200bb6de9220bd1dbab6bca
刘赛登 <liusd@tuya.com>
2020-11-25 17:36:34 +0800

    临时提交

--- leetcode/借雨水.js
+++ leetcode/借雨水.js
@@ -1,7 +1,7 @@
 const { max } = require("lodash")
 
 // 备忘录算法
-const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+let arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
 const maxL = []
 const maxR = []
@@ -29,3 +29,29 @@ console.log(ret)
 //https://github.com/labuladong/fucking-algorithm/blob/master/%E9%AB%98%E9%A2%91%E9%9D%A2%E8%AF%95%E7%B3%BB%E5%88%97/%E6%8E%A5%E9%9B%A8%E6%B0%B4.md
 
 
+// 双指针算法 nb
+
+arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+const length = arr.length
+let left = 0;
+let right = length - 1
+let l_max = arr[0]
+let r_max = arr[length - 1]
+
+let ret2 = 0
+while (left < right) {
+  l_max = Math.max(arr[left], l_max)
+  r_max = Math.max(arr[right], r_max)
+  console.log(arr[left], l_max, arr[right], r_max)
+  if (l_max < r_max) {
+    ret2 += l_max - arr[left]
+    left++
+    console.log(`Left = ${ret2}`)
+  } else {
+    ret2 += r_max - arr[right]
+    right--
+    console.log(`Right = ${ret2}`)
+  }
+}
+
+console.log(ret2)
