df161c27b9ca1bc0de1482fcc5b3b0bc6487a965
Shamao <994516742@qq.com>
2020-12-01 00:17:07 +0800

    临时提交

--- src/coins.js
+++ src/coins.js
@@ -1,11 +1,11 @@
-// 
-const minCoinCount = (coins = [2, 5, 10], target = 3) => {
+//
+const minCoinCount = (coins = [2, 5, 10], target = 4) => {
   const arr = new Array(target + 1).fill(target * 2)
   console.log(arr)
 
   arr[0] = 0
   for (let i = 1; i < target + 1; i++) {
-    for (j = 0; j < coins.length; j++) {
+    for (let j = 0; j < coins.length; j++) {
       if (i >= coins[j]) {
         console.log(i, coins[j], i - coins[j], arr[i - coins[j]])
         arr[i] = Math.min(arr[i], arr[i - coins[j]] + 1)
